5eebb1663336969da45d87dd750ee3daa4e2a7d9
tanetg <tanetg@gmail.com>
2018-02-27 13:14:22 +0700

    init toyarenotus project

---
+++ toyrnotus/src/main/app/web/app.config.js
@@ -0,0 +1,28 @@
+
+app.config(['$routeProvider',
+function config($routeProvider) {
+
+  $routeProvider
+    .when('/search', {
+      templateUrl: 'pages/search/search.html',
+      controller: 'search-controller'
+    })
+    .when('/toys-detail', {
+        templateUrl: 'pages/toys-detail/toys-detail.html',
+        controller: 'toys-detail-controller'
+    })
+    .when('/shopping-cart', {
+        templateUrl: 'pages/shopping-cart/shopping-cart.html',
+        controller: 'shopping-cart-controller'
+    })
+    .when('/shipping-detail', {
+        templateUrl: 'pages/shipping-detail/shipping-detail.html',
+        controller: 'shipping-detail-controller'
+    })
+    .when('/order-summary', {
+        templateUrl: 'pages/order-summary/order-summary.html',
+        controller: 'order-summary-controller'
+    })
+    .otherwise('/search');
+}
+]);
---
+++ toyrnotus/src/main/app/web/pages/shopping-cart/shopping-cart.controller.js
@@ -0,0 +1,3 @@
+app.controller('shopping-cart-controller', ['$scope', function ($scope) {
+    $scope.controllerName = 'shopping-cart-controller';
+}]);
