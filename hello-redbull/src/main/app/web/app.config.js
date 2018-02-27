768fb4d97d404aa3d9eb889bb3847b0aea09a062
tanat <tanatloke@gmail.com>
2018-02-27 11:34:45 +0700

    change configuration to be able to find frontend structure correctly.

--- hello-redbull/src/main/app/web/app.config.js
+++ hello-redbull/src/main/app/web/app.config.js
@@ -4,23 +4,23 @@ function config($routeProvider) {
 
   $routeProvider
     .when('/search', {
-      templateUrl: '/pages/search/search.html',
+      templateUrl: 'pages/search/search.html',
       controller: 'search-controller'
     })
     .when('/toys-detail', {
-        templateUrl: '/pages/toys-detail/toys-detail.html',
+        templateUrl: 'pages/toys-detail/toys-detail.html',
         controller: 'toys-detail-controller'
     })
     .when('/shopping-cart', {
-        templateUrl: '/pages/shopping-cart/shopping-cart.html',
+        templateUrl: 'pages/shopping-cart/shopping-cart.html',
         controller: 'shopping-cart-controller'
     })
     .when('/shipping-detail', {
-        templateUrl: '/pages/shipping-detail/shipping-detail.html',
+        templateUrl: 'pages/shipping-detail/shipping-detail.html',
         controller: 'shipping-detail-controller'
     })
     .when('/order-summary', {
-        templateUrl: '/pages/order-summary/order-summary.html',
+        templateUrl: 'pages/order-summary/order-summary.html',
         controller: 'order-summary-controller'
     })
     .otherwise('/search');
