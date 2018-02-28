99d7b3dacc39c6ff8dd62aa7c75af03bc24deaa6
tanat <tanatloke@gmail.com>
2018-02-28 16:58:18 +0700

    modify shopping cart, add button and quantity select

--- toyrnotus/src/main/app/web/pages/shopping-cart/shopping-cart.controller.js
+++ toyrnotus/src/main/app/web/pages/shopping-cart/shopping-cart.controller.js
@@ -5,22 +5,35 @@ app.controller('shopping-cart-controller', function ($scope, $routeParams, toySe
 
     $scope.shoppingCart = toyService.orders.orderLineItems;
 
-    $scope.shoppingCart.forEach((item, index) => {
+    $scope.removeItemFromCart = function(toyId){
+        console.log($scope.shoppingCart);
+        $scope.shoppingCart = $scope.shoppingCart.filter(function(orderItem) {
+            console.log('removing');
+            console.log(orderItem.toyId);
+            console.log(toyId);
+            return orderItem.toyId == toyId;
+        });
+        console.log($scope.shoppingCart);
+    }
 
-        toyService.getToy(item.toyId).then(
-            function (result) {
-                console.log(result);
-                console.log(index);
-                console.log($scope.shoppingCart[index]);
-                $scope.shoppingCart[index].brand = result.data.brand;
-                $scope.shoppingCart[index].age = result.data.age;
-                $scope.shoppingCart[index].gender = result.data.gender;
+    $scope.range = function (n) {
+        return new Array(n);
+    };
+    // $scope.shoppingCart.forEach((item, index) => {
 
-            }, function (err) {
-                //alert(err);
-                console.log(err);
-            }
-        );
+    //     toyService.getToy(item.toyId).then(
+    //         function (result) {
+    //             console.log(result);
+    //             console.log(index);
+    //             console.log($scope.shoppingCart[index]);
+    //             $scope.shoppingCart[index].brand = result.data.brand;
+    //             $scope.shoppingCart[index].age = result.data.age;
+    //             $scope.shoppingCart[index].gender = result.data.gender;
 
-    });
+    //         }, function (err) {
+    //             //alert(err);
+    //             console.log(err);
+    //         }
+    //     );
+    // });
 });
