1d6734267286ef2d5c4a559f7c6ed548bcbc2436
tanat <tanatloke@gmail.com>
2018-02-28 15:34:58 +0700

    add function responding to add item to cart button and implement change path to shopping cart page

--- toyrnotus/src/main/app/web/pages/toys-detail/toys-detail.controller.js
+++ toyrnotus/src/main/app/web/pages/toys-detail/toys-detail.controller.js
@@ -1,10 +1,10 @@
-app.controller('toys-detail-controller', function ($scope, $routeParams, toyService) {
+app.controller('toys-detail-controller', function ($scope, $routeParams, toyService, $location) {
 
     $scope.controllerName = 'toys-detail-controller';
     $scope.toyId = $routeParams.toyId;
     $scope.toy = {};
 
-    
+
     toyService.getToy($scope.toyId).then(
         function (result) {
             console.log(result);
@@ -16,7 +16,18 @@ app.controller('toys-detail-controller', function ($scope, $routeParams, toyServ
         }
     );
 
-    $scope.range = function(n) {
+    $scope.addToysToCart = function () {
+        let toyOrder = {
+            "toyId": $scope.toy.toyId,
+            "quantity": $scope.quantitySelect,
+            "unitPrice": $scope.toy.price
+        };
+        //TODO consolidate same toy id in order
+        toyService.orders.orderLineItems.push(toyOrder);
+        $location.path('/shopping-cart');
+    }
+
+    $scope.range = function (n) {
         return new Array(n);
     };
 
